e7c00475d32f4b54241715e6eb903a578a592503
rockybhanu <rampooja245@gmail.com>
2024-07-28 21:50:15 +0530

    all jobs updated db wp b-r

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 		"RESTIC_HOSTNAME",
 		"BACKUP_MOUNT_PATH",
 		"RESTORE_MOUNT_PATH",
+		"BACKUP_TYPE",
 	}
 
 	for _, v := range envVars {
@@ -36,24 +37,35 @@ func main() {
 		}
 	}
 
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlPort := os.Getenv("MYSQL_PORT")
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+	backupType := os.Getenv("BACKUP_TYPE")
 	action := os.Getenv("ACTION")
 
-	logger.Printf("Environment Variables: MYSQL_HOST=%s, MYSQL_PORT=%s, MYSQL_USER=%s, MYSQL_PASSWORD=%s, ACTION=%s",
-		mysqlHost, mysqlPort, mysqlUser, mysqlPassword, action)
+	logger.Printf("Environment Variables: BACKUP_TYPE=%s, ACTION=%s",
+		backupType, action)
 
 	// Perform the action
 	switch action {
 	case "backup":
 		initializeRepository(logger)
 		logSnapshots(logger)
-		performBackup(logger, mysqlHost, mysqlPort, mysqlUser, mysqlPassword)
+		if backupType == "db" {
+			performDBBackup(logger)
+		} else if backupType == "pvc" {
+			performPVCBackup(logger)
+		} else {
+			logger.Printf("Invalid BACKUP_TYPE: %s\n", backupType)
+			os.Exit(1)
+		}
 	case "restore":
 		logSnapshots(logger)
-		performRestore(logger, mysqlHost, mysqlPort, mysqlUser, mysqlPassword)
+		if backupType == "db" {
+			performDBRestore(logger)
+		} else if backupType == "pvc" {
+			performPVCRestore(logger)
+		} else {
+			logger.Printf("Invalid BACKUP_TYPE: %s\n", backupType)
+			os.Exit(1)
+		}
 	default:
 		logger.Printf("Invalid ACTION: %s\n", action)
 		os.Exit(1)
@@ -88,11 +100,20 @@ func logSnapshots(logger *log.Logger) {
 	logger.Println(string(output))
 }
 
-func performBackup(logger *log.Logger, host, port, user, password string) {
+func performDBBackup(logger *log.Logger) {
 	logger.Println("Performing database backup...")
 
+	// Retrieve database environment variables
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+
+	logger.Printf("Environment Variables: MYSQL_HOST=%s, MYSQL_PORT=%s, MYSQL_USER=%s, MYSQL_PASSWORD=%s",
+		mysqlHost, mysqlPort, mysqlUser, mysqlPassword)
+
 	// Execute mysqldump command
-	cmd := exec.Command("mysqldump", "-h", host, "-P", port, "-u", user, "-p"+password, "--all-databases")
+	cmd := exec.Command("mysqldump", "-h", mysqlHost, "-P", mysqlPort, "-u", mysqlUser, "-p"+mysqlPassword, "--all-databases")
 	output, err := cmd.CombinedOutput()
 
 	// Log and handle errors
@@ -127,7 +148,7 @@ func performBackup(logger *log.Logger, host, port, user, password string) {
 	os.Exit(0)
 }
 
-func performRestore(logger *log.Logger, host, port, user, password string) {
+func performDBRestore(logger *log.Logger) {
 	logger.Println("Performing database restore...")
 
 	// Ensure the latest snapshot is used for restoration unless specified otherwise
@@ -158,8 +179,17 @@ func performRestore(logger *log.Logger, host, port, user, password string) {
 	logger.Println("Backup file content:")
 	logger.Println(string(input))
 
+	// Retrieve database environment variables
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+
+	logger.Printf("Environment Variables: MYSQL_HOST=%s, MYSQL_PORT=%s, MYSQL_USER=%s, MYSQL_PASSWORD=%s",
+		mysqlHost, mysqlPort, mysqlUser, mysqlPassword)
+
 	// Execute mysql command
-	cmd := exec.Command("mysql", "-h", host, "-P", port, "-u", user, "-p"+password)
+	cmd := exec.Command("mysql", "-h", mysqlHost, "-P", mysqlPort, "-u", mysqlUser, "-p"+mysqlPassword)
 	cmd.Stdin = bytes.NewReader(input)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -172,5 +202,45 @@ func performRestore(logger *log.Logger, host, port, user, password string) {
 	}
 
 	logger.Println("Restore successful")
+	os.Exit(0)
+}
+
+func performPVCBackup(logger *log.Logger) {
+	logger.Println("Starting PVC backup...")
+
+	logger.Printf("Starting backup of %s to %s with host %s", os.Getenv("BACKUP_MOUNT_PATH"), os.Getenv("RESTIC_REPOSITORY"), os.Getenv("RESTIC_HOSTNAME"))
+	backupCmd := exec.Command("restic", "-r", os.Getenv("RESTIC_REPOSITORY"), "--host", os.Getenv("RESTIC_HOSTNAME"), "backup", os.Getenv("BACKUP_MOUNT_PATH"))
+	backupOutput, err := backupCmd.CombinedOutput()
+	if err != nil {
+		logger.Printf("Restic backup failed: %v\n", err)
+		logger.Println(string(backupOutput))
+		os.Exit(1)
+	}
+	logger.Println("Backup completed successfully.")
+	logger.Println(string(backupOutput))
+	logSnapshots(logger)
+	os.Exit(0)
+}
+
+func performPVCRestore(logger *log.Logger) {
+	logger.Println("Starting PVC restore...")
+
+	// Ensure the latest snapshot is used for restoration unless specified otherwise
+	snapshotID := os.Getenv("SNAPSHOT_ID")
+	if snapshotID == "" {
+		snapshotID = "latest"
+	}
+
+	logger.Printf("Starting restore of %s to %s with host %s", snapshotID, os.Getenv("RESTORE_MOUNT_PATH"), os.Getenv("RESTIC_HOSTNAME"))
+	restoreCmd := exec.Command("restic", "-r", os.Getenv("RESTIC_REPOSITORY"), "--host", os.Getenv("RESTIC_HOSTNAME"), "restore", snapshotID, "--target", os.Getenv("RESTORE_MOUNT_PATH"))
+	restoreOutput, err := restoreCmd.CombinedOutput()
+	if err != nil {
+		logger.Printf("Restic restore failed: %v\n", err)
+		logger.Println(string(restoreOutput))
+		os.Exit(1)
+	}
+	logger.Println("Restore completed successfully.")
+	logger.Println(string(restoreOutput))
+	logSnapshots(logger)
 	os.Exit(0)
 }
